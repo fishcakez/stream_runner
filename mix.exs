fd0bb1e7d4415f384c8fea4029cd03baa02283ce
James Fish <james@fishcakez.com>
2015-08-04 00:28:57 +0100

    Bump to 1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule StreamRunner.Mixfile do
 
   def project do
     [app: :stream_runner,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
